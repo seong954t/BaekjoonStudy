8070ab2b82d15378ec178df05ffba4132c8b6eb5
seong954t <seomtae@naver.com>
2018-04-02 14:41:34 +0900

    1011_Fly me to the Alpha Centauri

---
+++ src/C/1011_kst/main.c
@@ -0,0 +1,33 @@
+//
+//  main.c
+//  10430_kst
+//
+//  Created by 김승태 on 2018. 4. 2..
+//  Copyright © 2018년 김승태. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int T, start, end;
+    scanf("%d", &T);
+    for (int i = 0; i < T; i++){
+        scanf("%d %d", &start, &end);
+        int length = end - start, j = 0;
+        unsigned int data = 0;
+        while (length > data*2){
+            j++;
+            data += j;
+        }
+        data *= 2;
+        data -= j - 1;
+        if (length < data){
+            printf("%d\n", 2 * j -1);
+        }
+        else{
+            printf("%d\n", 2 * j);
+        }
+    }
+    return 0;
+}
