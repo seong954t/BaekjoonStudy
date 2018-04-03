c45ec7f092055b1d4c86123be88bb015950a51d4
seong954t <seomtae@naver.com>
2018-04-03 13:30:41 +0900

    1000_A+B

---
+++ src/C/1000_kst/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  10430_kst
+//
+//  Created by 김승태 on 2018. 4. 3..
+//  Copyright © 2018년 김승태. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%d", A+B);
+    return 0;
+}
