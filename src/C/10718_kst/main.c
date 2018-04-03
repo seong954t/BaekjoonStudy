577d1d3040aa35d2406c46cb0d8861484b6b831c
seong954t <seomtae@naver.com>
2018-04-03 13:34:51 +0900

    10718_We love kriii

---
+++ src/C/10718_kst/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  10430_kst
+//
+//  Created by 김승태 on 2018. 4. 3..
+//  Copyright © 2018년 김승태. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("강한친구 대한육군\n강한친구 대한육군");
+    return 0;
+}
