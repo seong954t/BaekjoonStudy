d59eeb59455248c15789c5d24bd770e0696ce72a
seong954t <seomtae@naver.com>
2018-04-04 14:52:08 +0900

    2557_Hello World

---
+++ src/C/2557_kst/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  10430_kst
+//
+//  Created by 김승태 on 2018. 4. 4..
+//  Copyright © 2018년 김승태. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello World!");
+    return 0;
+}
