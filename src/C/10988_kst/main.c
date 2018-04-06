b209378d81d580e40d615941c84ccd6415e0d2a3
seong954t <seomtae@naver.com>
2018-04-06 13:33:51 +0900

    10988_팰린드롬인지 확인하기

---
+++ src/C/10988_kst/main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  10988_kst
+//
+//  Created by 김승태 on 2018. 4. 5..
+//  Copyright © 2018년 김승태. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    char str[101];
+    int len;
+    char check = 1;
+    scanf("%s", str);
+    len = strlen(str);
+    for(int i = 0; i<len; i++){
+        if(str[i] != str[len-i-1]){
+            check = 0;
+            break;
+        }
+    }
+    printf("%d", check);
+    return 0;
+}
