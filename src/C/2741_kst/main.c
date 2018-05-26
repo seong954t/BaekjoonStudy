f0321e41f8de3dc3e7d3fbd73e53fe61480f9947
seong954t <seomtae@naver.com>
2018-05-26 13:32:08 +0900

    2741_N 찍기

---
+++ src/C/2741_kst/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  2741_kst
+//
+//  Created by 김승태 on 2018. 5. 26..
+//  Copyright © 2018년 김승태. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int A, i;
+    scanf("%d", &A);
+    for(i = 1; i<A+1; i++){
+        printf("%d\n", i);
+    }
+}
