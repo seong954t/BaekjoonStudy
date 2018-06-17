9bdb260dca91ce0f4ab94570a662604e3e697d28
seong954t <seomtae@naver.com>
2018-06-17 22:12:18 +0900

    11654_아스키 코드

---
+++ src/C/11654_kst/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  11654_kst
+//
+//  Created by 김승태 on 2018. 6. 17..
+//  Copyright © 2018년 김승태. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    char s;
+	scanf("%c", &s);
+	printf("%d", s);
+}
