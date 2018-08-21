22eee4f3a2c1786b70ff0d99842dd71ca85f6b50
seong954t <seomtae@naver.com>
2018-08-21 19:56:20 +0900

    11050_이항 계수 1

---
+++ src/C/11050_이항 계수 1/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  111050_kst
+//
+//  Created by 김승태 on 2018. 8. 21..
+//  Copyright © 2018년 김승태. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int result = 1, N, K;
+	scanf("%d %d", &N, &K);
+	for (int i = N; i > N - K; i--){
+		result *= i;
+	}
+	for (int i = K; i > 0; i--){
+		result /= i;
+	}
+	printf("%d", result);
+}
