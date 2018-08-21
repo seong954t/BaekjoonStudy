fb7dfa64abf5f165a7e38cc94b9c4d97d6b6e0a7
seong954t <seomtae@naver.com>
2018-08-21 19:54:23 +0900

    10172_개

---
+++ src/C/10172_개/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  10172_kst
+//
+//  Created by 김승태 on 2018. 8. 21..
+//  Copyright © 2018년 김승태. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+}
